b5c00a60676ec4384b3a9123b643882339bc1dad
Justin Scaman <fr33th3n3t.quft9@passmail.net>
2024-11-25 22:14:16 -0500

    remove debug statement and fix cart tile

--- assets/contact-form-custom.js
+++ assets/contact-form-custom.js
@@ -1,1 +1,25 @@
 //logic for our custom contact form
+console.log('custom js form...')
+// businessAccount
+//
+//
+//
+//
+//
+//
+
+document.addEventListener('DOMContentLoaded', () => {
+    const isBusinessAcct = document.querySelector('#businessAccount');
+    const companyName = document.querySelector('#companyName');
+    // surrounding select element
+    const contactReason = document.querySelector('#contactReason');
+    
+    isBusinessAcct.addEventListener('change', (event) => {
+        if (event.target.checked) {
+            companyName.classList.remove('disabled');
+        } else {
+            companyName.classList.add('disabled');
+        }
+    });
+});
+
